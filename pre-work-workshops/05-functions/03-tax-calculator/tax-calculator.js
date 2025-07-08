3db6d3a31fb8a49248a8a2bd3a24813fafe5313c
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-08 16:55:47 +0545

    tax-calculator completed

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function taxCalculator(amount,city){
+ if(city==='NY'){
+    return amount+0.04*amount;
+ }else{
+   return amount+0.06625*amount;
+ }
+}
+console.log(taxCalculator(100,'NJ'))
