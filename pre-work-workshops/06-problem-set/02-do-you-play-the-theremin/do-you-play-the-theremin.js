a1d7120d97fb1550354f3513f691aabdc6b85c73
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-08 18:26:47 +0545

    do-you-play-the-theremin completed

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function doYouPlayTheTheremin(stringg){
+    if(stringg[0].toLowerCase()==='s'){
+        return true
+    }else{
+       return false
+    }
+}
+console.log(doYouPlayTheTheremin('Srank'))
