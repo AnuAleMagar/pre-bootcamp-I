fb61beebf42b07f38ac711c744d9f4fb123d4f11
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-08 12:34:57 +0545

    take the shortcut completed

--- pre-work-workshops/03-expressions-variables-and-tdd/05-take-the-shortcut/take-the-shortcut.js
+++ pre-work-workshops/03-expressions-variables-and-tdd/05-take-the-shortcut/take-the-shortcut.js
@@ -1,3 +1,11 @@
 let happyNum = 1000;
 
 // YOUR CODE BELOW
+happyNum -=900
+console.log(happyNum)
+happyNum /=5
+console.log(happyNum)
+happyNum /=5
+console.log(happyNum)
+happyNum +=1
+console.log(happyNum)
