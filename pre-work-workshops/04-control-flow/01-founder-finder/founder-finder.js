419f1191353e13e664775cfc872abd54cd9f548d
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-08 16:30:02 +0545

    founder finder completed

--- pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -1,7 +1,13 @@
-let name = 'David';
+// let name = 'David';
 // let name = 'Nimit';
-// let name = 'Someone else';
-
-let found;
+let name = 'Someone else';
 
 // YOUR CODE BELOW
+
+let found;
+if(name==='David' || name==='Nimit'){
+    found=true;
+}else{
+    found=false;
+}
+console.log(found)
