ce02466936e09164b5c09caf7c1500244a7b7d76
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-08 18:44:32 +0545

    maxOfThree completed

--- pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function maxOfThree(num1,num2,num3){
+ if(num1>=num2 && num1 >=num3){
+    return Number(num1);
+ }else if(num2>=num3 && num2>=num1){
+    return Number(num2);
+ }
+ else{
+   return Number(num3);
+ }
+}
+console.log(maxOfThree(20,40,10))
