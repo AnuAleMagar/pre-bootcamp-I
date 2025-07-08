a225ada86c7ee28ab7466584a7867d2596bdf67e
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-08 18:08:52 +0545

    my-mnemonic completed

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+function myMnemonic(string1,string2,string3,string4){
+let str=''
+if(string1!=undefined){
+    str+=string1[0]
+}
+if(string2!=undefined){
+    str+=string2[0]
+    
+}
+if(string3!=undefined){
+    str+=string3[0]
+  
+}
+if(string4!=undefined){
+    str+=string4[0]
+ 
+}
+ return str;
+}
+
+console.log(myMnemonic(''))
