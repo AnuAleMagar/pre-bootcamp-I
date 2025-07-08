de0a1268f84eb874fe5f211d123575d2096599eb
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-08 18:31:58 +0545

    last-character completed

--- pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function lastCharacter(str1,str2){
+    let len1=str1.length-1
+    let len2=str2.length-1
+  
+    if(str1[len1]===str2[len2]){
+        return true;
+    }else{
+        return false
+    }
+}
