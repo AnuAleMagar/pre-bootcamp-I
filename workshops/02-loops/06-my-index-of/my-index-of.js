7f6ace0d58202b84c396f2779204461387ca1015
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-09 16:26:59 +0545

    myIndexOf completed

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function myIndexOf(source,searchValue,startIdx=0){
+ 
+    for(let i=startIdx;i<source.length;i++){
+        for(let j=0;j<searchValue.length;j++){
+          
+            if(source[i+j]==searchValue[j]){
+                if(j==searchValue.length-1){
+                return i;
+                }
+            }else{
+                break;
+            }
+        }
+    }
+    return -1;
+}
+console.log(myIndexOf('hello', 'h'))
+console.log(myIndexOf('twice twice', 'ice'))
+console.log(myIndexOf('twice twice', 'ice', 5))
+console.log(myIndexOf('happy string', 'sad'))
