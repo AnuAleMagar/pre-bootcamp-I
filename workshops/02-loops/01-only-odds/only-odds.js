2ce3e116b6dc791f1b5be2b0f80d919cb382c54b
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-09 12:24:17 +0545

    onlyOdds completed

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function onlyOdds(num){
+    let sum=0
+    if(num<0){
+        return sum;
+    }else{
+        for(let i=num;i>0;i--){
+            if(i%2!=0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
+console.log(onlyOdds(5))
