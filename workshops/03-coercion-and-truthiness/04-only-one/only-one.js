019a6cabe421b9191800ff8c3a32cdc78d3f1592
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-09 17:56:09 +0545

    onlyOne completed

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,44 @@
+
+/*
+### Only One
+
+Write a function `onlyOne` that accepts three arguments of any type.
+
+`onlyOne` should return true only if exactly one of the three arguments are
+truthy. Otherwise, it should return false.
+
+Do not use the equality operators (`==` and `===`) in your solution.
+
+```javascript
+onlyOne(false, false, true); // => true
+onlyOne(0, 1, 2) // => false
+```
+
+*/
 // YOUR CODE BELOW
+function onlyOne(a,b,c){
+    if(a){
+        if(!b){
+            if(!c){
+                return true
+            }else{
+                return false
+            }
+        }else{
+            return false
+        }
+    }else if(b){
+        if(!c){
+            return true
+        }else{
+            return false
+        }
+    }else if(c){
+        return true
+    } else{
+        return false;
+    }
+
+}
+console.log(onlyOne(0, 1, 2))
+console.log(onlyOne(false, false, true))
