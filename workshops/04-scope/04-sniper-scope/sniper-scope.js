8c99afb5a65629485940c4c75b227c887d3f2431
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-09 20:12:50 +0545

    sniper-scope completed

--- workshops/04-scope/04-sniper-scope/sniper-scope.js
+++ workshops/04-scope/04-sniper-scope/sniper-scope.js
@@ -1,16 +1,29 @@
+/*
+### Sniper Scope
+
+Each of the functions in sniper-scope.js asks you to make a prediction: will
+AgentinScope be equal to true or false? The tests for this problem will pass if
+your prediction is correct. Note that the tests each of these functions in the
+same order in which they're defined in sniper-scope.js.
+
+It's best to get each test to pass before moving onto the next function. If you
+get a prediction wrong, try to understand the observed behavior before moving
+on.
+
+*/
 let bond = '007';
 
 function oddJob() {
+  debugger;
   let AgentinScope = (bond === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction =true; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
 
-
 function goldFinger(bond) {
   let AgentinScope = (bond === '007')
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = false; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
@@ -20,7 +33,7 @@ function scaramanga(target) {
   target = bond;
 
   let AgentinScope = (bond === '007')
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = true; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
@@ -30,7 +43,7 @@ function drNo() {
   let bond = 'Body Double';
 
   let AgentinScope = (bond === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = false; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
@@ -39,9 +52,8 @@ function drNo() {
 function jaws(agent) {
   agent = bond;
   bond = 'Body Double';
-
   let AgentinScope = (agent === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = true; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
@@ -50,9 +62,9 @@ function jaws(agent) {
 function elChiffre() {
   let agent = bond;
   bond = 'Body Double';
-
+  
   let AgentinScope = (agent === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let prediction = false; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
