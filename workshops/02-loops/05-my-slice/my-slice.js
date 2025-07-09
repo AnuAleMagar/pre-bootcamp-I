38f433a346df2abbd3e9f8b4d55c12f29f85c4e1
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-09 13:05:26 +0545

    mySlice completed

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function mySlice(originalString,startIdx,endIdx){
+ let result=''
+ if(startIdx===undefined){
+    return originalString;
+ }
+ if(endIdx===undefined){
+    for(let i=startIdx;i<originalString.length;i++)
+        result+=originalString[i]
+ }else{
+     for(let i=startIdx;i<endIdx;i++)
+        result+=originalString[i]
+ }
+ return result;
+}
+console.log(mySlice('slice and dice',2))
