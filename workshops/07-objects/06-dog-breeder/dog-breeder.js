776a9fdd87ad4754961bf2a957e4eb41ce24e508
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-10 21:07:09 +0545

    completed

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,32 @@
+
+/*
+### Dog Breeder
+
+Define a function, `dogBreeder`, that accepts up to two optional arguments:
+  1. name (string)
+  2. age (number)
+
+`dogBreeder` should return an object that represents a new dog! If the user
+doesn't define a name, assume the dog's name is Steve. If the user doesn't
+define the dog's age, assume the dog's age is 0.
+
+```javascript
+dogBreeder('Sam', 12)    // => {name: 'Sam', age: 12}
+
+dogBreeder(15)    // => {name:'Steve', age: 15}
+```
+*/
 // YOUR CODE BELOW
+function dogBreeder(name='Steve',age=0){
+  let newDog={}
+ if (typeof(name)==="number"){
+    newDog.name="Steve"
+    newDog.age=name;
+ }else{
+     newDog.name=name;
+      newDog.age=age;
+ }
+
+ return newDog
+}
+console.log(dogBreeder(12) )
--- workshops/07-objects/07-attendance-check/attendance-check.js
+++ workshops/07-objects/07-attendance-check/attendance-check.js
@@ -1,4 +1,21 @@
-let classRoom = [
+
+/*
+### Attendance Check
+
+Define a function, `attendanceCheck`, that accepts a day of the week as a string.
+
+Using the globally-defined classRoom array, `attendanceCheck` should return a
+new array with only the names of the students present on the inputted day of
+the week.
+
+```javascript
+
+classCheck('Monday'); // => ['Marnie', 'Shoshanna']
+
+classCheck('Wednesday'); // => ['Marnie', 'Lena']
+```
+*/
+let classRoom = [ //[{"key":[{}]}]
     {
         "Marnie" : [
                 {"Monday" : true},
@@ -38,3 +55,17 @@ let classRoom = [
 ];
 
 // YOUR CODE BELOW
+function attendanceCheck(day){
+    let newArray=[]
+    for(let i=0;i<classRoom.length;i++){
+        let keys=Object.keys(classRoom[i])
+        let a=classRoom[i][keys]
+        for(let key of a){
+            if(key[day]){
+                newArray.push(keys[0])
+            }
+        }
+     } 
+    return newArray
+}
+console.log(attendanceCheck("Friday"));
