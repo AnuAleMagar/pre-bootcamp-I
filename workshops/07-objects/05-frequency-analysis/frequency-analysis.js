af23151d6f9f58f7008447e04b57bd921c876f33
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-10 20:06:20 +0545

    frequencyAnalysis completed

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,25 @@
+/*
+### Frequency Analysis
+
+Define a function `frequencyAnalysis` that accepts a string of lower-case
+letters.
+
+`frequencyAnalysis` should return an object containing the count for each letter
+in the string.
+
+```javascript
+frequencyAnalysis('abca'); // => {a: 2, b: 1, c: 1}
+```
+*/
 // YOUR CODE BELOW
+function frequencyAnalysis(string){
+  let result={}
+  for(let i=0;i<string.length;i++){
+    result[string[i]]=0
+  }
+  for(let i=0;i<string.length;i++){
+    result[string[i]]+=1
+  }
+  return result
+}
+console.log(frequencyAnalysis('abcaacaa'))
