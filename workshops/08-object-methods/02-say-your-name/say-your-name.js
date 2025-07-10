c3c7a0f47a2ebf16231ee73533f51c586e529dc6
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-10 21:43:58 +0545

    Say Your Name completed

--- workshops/08-object-methods/02-say-your-name/say-your-name.js
+++ workshops/08-object-methods/02-say-your-name/say-your-name.js
@@ -1,1 +1,27 @@
+
+/*
+### Say Your Name
+
+Create a new object, `me2`. Expand the `myGreeting` method from me so it accepts
+another object with a name property. The returned greeting should now greet the
+other object by name.
+
+```javascript
+console.log(me2.name); // 'Tarana'
+console.log(you.name); // 'Alyssa'
+me2.getGreeting(you); // => 'Hi Alyssa, my name is Tarana.'
+```
+
+*/
 // YOUR CODE BELOW
+const me2={
+  name:'Tarana',
+  getGreeting:function(obj){
+    return `Hi ${obj.name}, my name is ${this.name}.`
+  }
+}
+const you={
+  name:'Alyssa'
+}
+console.log(me2.getGreeting(you)); 
+// => 'Hi Alyssa, my name is Tarana.'
