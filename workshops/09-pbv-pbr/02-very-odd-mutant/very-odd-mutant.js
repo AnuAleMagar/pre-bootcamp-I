8255d908652fdf19dc0309c9ae299e8ff569eb6d
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-11 11:44:28 +0545

    veryOddMutant completed

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,1 +1,35 @@
+
+/*
+### Very Odd Mutant
+'
+Write a function, 'veryOddMutant'. The function accepts an array of numbers.
+The function should mutate the given array by replacing every even number in the
+array with the string 'normie'.
+
+'veryOddMutant' should return a count of the number of even numbers it replaced.
+
+```javascript
+let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+let countRemoved = veryOddMutant(allTheNums);
+
+console.log('allTheNums:', allTheNums);
+// [1, 'normie', 3, 'normie', 5, 'normie', 7, 'normie'];
+
+console.log('countRemoved:', countRemoved);
+// 4
+```
+
+*/
 // YOUR CODE BELOW
+function veryOddMutant(array){
+   let count=0;
+   for(let i=0;i<array.length;i++){
+    if(array[i]%2===0){
+        array[i]='normie'
+        count++;
+    }
+   }
+  return count
+}
+
+
