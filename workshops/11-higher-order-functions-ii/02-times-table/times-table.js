67ecd402c3e3654e340f49548c07cf2525e50638
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-13 12:02:22 +0545

    timesTable completed

--- workshops/11-higher-order-functions-ii/02-times-table/times-table.js
+++ workshops/11-higher-order-functions-ii/02-times-table/times-table.js
@@ -1,1 +1,29 @@
+
+/*
+### Times Tables
+
+Write a function that generates a 'Times Tables' function for any number passed
+in. The function should accept a single parameter (a number) and return a
+function that itself returns the product of that number and any number passed
+in.
+
+```javascript
+let ninesTable = timesTable(9);
+ninesTable(8)    // => 72
+
+let twelvesTable = timesTable(12);
+twelvesTable(100)    // => 1200
+```
+
+*/
 // YOUR CODE BELOW
+function timesTable(num){
+  return function(num2){
+         return num*num2
+  }
+}
+let ninesTable = timesTable(9);
+console.log(ninesTable(8) )   // => 72
+
+let twelvesTable = timesTable(12);
+console.log(twelvesTable(100) )   // => 1200
