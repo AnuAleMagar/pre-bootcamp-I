feadf2fddcd5746f7951fc6b85110e8195e3a7eb
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-13 16:52:40 +0545

    sumDigits completed

--- workshops/12-recursion-i/06-sum-digits/sum-digits.js
+++ workshops/12-recursion-i/06-sum-digits/sum-digits.js
@@ -1,1 +1,21 @@
+
+/*
+### Sum Digits
+
+Write a function, sumDigits, that sums all the digits of a given integer
+(assume positive). Use recursion.
+
+```javascript
+sumDigits(1234) // => 10
+```
+
+*/
 // YOUR CODE BELOW
+const sumDigits=function(number){
+    if((number/10)<1){
+        return number;
+    }else{
+        return (number%10+sumDigits(parseInt(number/10)))
+    }
+}
+console.log(sumDigits(12346)) // => 10)
