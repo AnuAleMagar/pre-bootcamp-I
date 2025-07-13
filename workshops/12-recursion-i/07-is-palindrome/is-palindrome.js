388cd861d77f92e317dc7a5873250f8e6fdea920
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-13 16:58:26 +0545

    isPalindrome completed

--- workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
+++ workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
@@ -1,1 +1,32 @@
+
+/*
+### Is Palindrome
+
+A palindrome is a word that is spelled the same forward and backward. For
+example, "LEVEL", "RACECAR", and "KAYAK" are all palindromes, while "MOTOR",
+"RUDDER", and "DOGGED" are not palidromes.
+
+Write a recursive function, isPalindrome, to check if a string is a palindrome
+or not.
+
+```javascript
+isPalindrome('Kayak'); // => true
+isPalindrome('NEVERODDOREVEN'); // => true
+isPalindrome('Tacocat'); // => true
+isPalindrome('SELFLESS'); // => false
+```
+
+*/
 // YOUR CODE BELOW
+const isPalindrome=function(string){
+    if(string==='' ||string.length===1){
+        return true
+    }else{
+        let condition=(string[0].toLowerCase()===string[string.length-1].toLowerCase())
+        return condition && isPalindrome(string.slice(1,-1))
+    }
+}
+console.log(isPalindrome('Kayak')); // => true
+console.log(isPalindrome('NEVERODDOREVEN')); // => true
+console.log(isPalindrome('Tacocat')); // => true
+console.log(isPalindrome('SELFLESS')); // => false
