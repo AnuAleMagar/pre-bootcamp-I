0d0baaa6da357c47f5d7c3c7a5a0502c04ca355d
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-13 16:21:54 +0545

    sumNums completed

--- workshops/12-recursion-i/03-sum-nums/sum-nums.js
+++ workshops/12-recursion-i/03-sum-nums/sum-nums.js
@@ -1,1 +1,22 @@
+
+/*
+### Sum Nums
+
+Write a function, sumNums, that takes a number greater than 1 and returns the
+sum of all the numbers between the given number and 1. Use recursion.
+
+
+```javascript
+sumNums(3); // => 6 (3 + 2 + 1)
+```
+
+*/
 // YOUR CODE BELOW
+function sumNums(num){
+   if(num===1){
+    return num
+   }else{
+    return num+sumNums(num-1)
+   }
+}
+console.log(sumNums(3));
