29e6595552c59bb0ea9260a3d17118ab3d552a6b
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-13 16:17:16 +0545

    backwardString completed

--- workshops/12-recursion-i/02-backward-string/backward-string.js
+++ workshops/12-recursion-i/02-backward-string/backward-string.js
@@ -1,1 +1,33 @@
-// YOUR CODE BELOW
+
+/*
+### Backward String
+
+Write a function, backwardString, that receives a string. It should log every
+letter in the string, from the last character to the first. Use recursion.
+
+NOTE: As with the last problem, the tests will check how many times console.log
+is called.
+
+```javascript
+backwardString('happy');
+// y
+// p
+// p
+// a
+// h
+```
+
+*/
+// YOUR CODE BELOW happy
+function backwardString(string){
+    debugger
+     if(string===''){
+       return
+    }
+    console.log(string[string.length-1])
+    if(string===''){
+       return
+    }
+    backwardString(string.slice(0,-1))
+}
+backwardString("Apple")
