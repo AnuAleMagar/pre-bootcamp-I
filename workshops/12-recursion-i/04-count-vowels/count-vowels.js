0bc8cc1b3f56d42ba699aa1ba7a203ea9bbf08a3
Anu Ale Magar <anualemagar212@gmail.com>
2025-07-13 16:34:35 +0545

    countVowels completed

--- workshops/12-recursion-i/04-count-vowels/count-vowels.js
+++ workshops/12-recursion-i/04-count-vowels/count-vowels.js
@@ -1,1 +1,26 @@
+
+/*
+### Count Vowels
+
+Write a function, countVowels, that accepts a string and returns the number of
+vowels in that string. Use recursion.
+
+```javascript
+countVowels('Four score and seven years'); // => 9
+```
+
+*/
 // YOUR CODE BELOW
+function countVowels(string){
+    let vowels='aeiou'
+     if(string===''){
+        return 0
+    }
+    if(vowels.includes(string[0].toLowerCase())){
+        return 1+countVowels(string.slice(1));
+    }
+    else{
+        return(0+countVowels(string.slice(1)))
+    }
+}
+console.log(countVowels('Four score and seven years'))
